5abc3b258571977e3228fe98e05fa6a6b55fcf42
pcherkasova <polina.c@live.com>
2015-09-25 09:28:46 -0700

    small changes

--- app/server.routes.js
+++ app/server.routes.js
@@ -1,20 +1,14 @@
-// var explorer = require('../app/controllers/explorer.server.controller.js'); 
+var mongodbController = require('../app/controllers/mongodb.server.controller.js'); 
 
 module.exports = function (app) {
 	
+	// UI calls
 	app.get('/', function(req, res) {
 		// ejs render automatically looks in the views folder
 		res.render('index');
 	});
 
-
-	// //API calls
-    // app.route('/api/runQuery').get(explorer.runQueryHTML);
-	// app.route('/api/getCollections').get(explorer.getCollectionsHTML);
-
-    // //UI calls
-	// app.get('/', function (request, response) {
-	// 	response.sendFile(__dirname + '/views/mongo-explorer.html');
-	// 	//response.send('Hello World!');
-	// });
+	// API calls
+    app.route('/api/runQuery').get(mongodbController.runQueryHTML);
+	app.route('/api/getCollections').get(mongodbController.getCollectionsHTML);
 };
